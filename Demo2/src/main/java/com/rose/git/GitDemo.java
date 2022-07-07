599026e8bd9b692f1c4006fb376efe819af1f097
Rose <Rose@rose.com>
2022-07-07 13:31:46 +0800

    hot-fix commit2

--- Demo2/src/main/java/com/rose/git/GitDemo.java
+++ Demo2/src/main/java/com/rose/git/GitDemo.java
@@ -6,6 +6,7 @@ public class GitDemo {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4 hot-fix");
+        System.out.println("hello git hot-fix test");
 
     }
 }
